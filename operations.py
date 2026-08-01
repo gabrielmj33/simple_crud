52198f17dd16429a8c2e9dac9fd4c87e1f28c451
Gabriel Moraes <therealgolk@gmail.com>
2026-08-01 09:25:07 -0300

    configurating the bcrypt and .gitignore

--- operations.py
+++ operations.py
@@ -2,4 +2,12 @@ from database import conn
 import bcrypt
 
 def create_user(name,email,cpf,password):
-    connect =
+    connect = conn()
+    cursor = connect.cursor()
+
+    # transform the typed password in bytes
+    bytes_password = password.encode('utf-8')
+
+    #generate the "salt" and do the hash of the password
+    salt = bcrypt.gensalt()
+    hash_password = bcrypt.hashpw(bytes_password, salt)
