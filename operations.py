36539a5074864aa612d95373911454e4bf7fb4f5
Gabriel Moraes <therealgolk@gmail.com>
2026-08-03 08:00:00 -0300

    create new user function in operations

--- operations.py
+++ operations.py
@@ -11,3 +11,14 @@ def create_user(name,email,cpf,password):
     #generate the "salt" and do the hash of the password
     salt = bcrypt.gensalt()
     hash_password = bcrypt.hashpw(bytes_password, salt)
+    saved_password = hash_password.decode('utf-8')
+
+    cursor.execute('''
+        INSERT INTO clients (name, email, cpf, password)
+        VALUES (?, ?, ?, ?)
+        ''',
+        (name,email,cpf,saved_password)
+    )
+
+    connect.commit()
+    connect.close()
