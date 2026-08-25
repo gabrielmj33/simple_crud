c68f13ed6896d87f9072d0dc81a6b6019f224694
Gabriel Moraes <therealgolk@gmail.com>
2026-08-24 21:34:18 -0300

    Password option in menu

--- main.py
+++ main.py
@@ -5,15 +5,20 @@ def menu():
     print("\n--- NEW USER ---")
     name = input("Name: ").strip()
     if len(name) < 3:
-        print("The name have to be at least 3 characters")
+        print("The name have to be at least 3 characters.")
         return
 
     email = input("Email: ").strip()
     if "@" not in email or "." not in email:
-        print("Invalid  email format")
+        print("Invalid  email format.")
         return
 
     cpf = input("CPF (only numbers): ").strip()
     if len(cpf) != 11 or not cpf.isdigit():
-        print("The CPF has to have exactly 11 numbers")
+        print("The CPF has to have exactly 11 numbers.")
+        return
+
+    password = input("Password: ")
+    if len(password) < 6:
+        print("The password need have at least 6 characters.")
         return
